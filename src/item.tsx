9f16fab2278c14753d2cd5b59a12bef88b51a2dd
Hassan Javaid <h.blade@gmail.com>
2022-09-12 08:16:44 -0500

    Added Edit and Display Mode

--- src/item.tsx
+++ src/item.tsx
@@ -1,4 +1,5 @@
 import { css } from "solid-styled-components";
+import { createSignal, Show } from "solid-js";
 import type { Component} from "solid-js";
 
 type ItemProps = {
@@ -17,6 +18,7 @@ const ItemStyling = (complete: boolean) => css `
 
 export const Item: Component<ItemProps> = (props) => {
     const itemStyle = ItemStyling(props.todo.complete);
+    const [isEditing, setIsEditing] = createSignal(false);
     const changeComplete = () => {
         props.setTodos((todos: TodoItem[]) => {
             const newTodos = todos.map((todo) => props.todo === todo ? {...todo, complete: !todo.complete} : todo);
@@ -29,12 +31,36 @@ export const Item: Component<ItemProps> = (props) => {
             return newTodos;
         })
     }
+
+    const switchMode = (e: Event) => {
+        e.preventDefault();
+        setIsEditing(!isEditing())
+    }
+
+    const displayMode = () => (
+        <div>
+        {props.todo.text}
+            <button onClick={switchMode}>Edit</button>
+        <button onClick={deleteItem}>X</button>
+        </div>);
+
+    const editMode = () => (
+        <div>
+            <input
+                value={props.todo.text}
+            />
+            <button>Save</button>
+            <button onClick={switchMode}>Cancel</button>
+        </div>
+    );
+
     return (
         <li class={itemStyle}>
             <label>
                 <input type="checkbox" checked={props.todo.complete} onChange={changeComplete} />
-                {props.todo.text}
-                <button onClick={deleteItem}>X</button>
+                <Show when={isEditing()} fallback={displayMode()}>
+                    {editMode()}
+                </Show>
             </label>
         </li>
     )
